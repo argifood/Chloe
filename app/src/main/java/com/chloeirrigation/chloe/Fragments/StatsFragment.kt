689fdb811d58e5b317c32cd07622cd85d62c16e3
Loukas Pavlos Basios <l4grange@gmail.com>
2019-04-06 18:35:53 +0300

    Added charts Fragment Layout.

--- app/src/main/java/com/chloeirrigation/chloe/Fragments/StatsFragment.kt
+++ app/src/main/java/com/chloeirrigation/chloe/Fragments/StatsFragment.kt
@@ -2,11 +2,12 @@ package com.chloeirrigation.chloe.Fragments
 
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
 import com.chloeirrigation.chloe.R
+import kotlinx.android.synthetic.main.fragment_stats.*
 
 class StatsFragment : Fragment() {
 
@@ -26,12 +27,48 @@ class StatsFragment : Fragment() {
     }
 
     private fun setupSensorCharts() {
+        sensorSegmentedControl.addOnSegmentClickListener {
+            val position = it.absolutePosition
+            updateSensorChart(position)
+        }
 
+        sensorChart // TODO: 06/04/2019 Do something
+
+        updateSensorChart(0)
     }
+
 
     private fun setupWeatherCharts() {
+        weatherSegmentedControl.addOnSegmentClickListener {
+            val position = it.absolutePosition
+            updateWeatherChart(position)
+        }
 
+        weatherChart // TODO: 06/04/2019 Do something
+
+        updateSensorChart(0)
     }
 
+
+
+    private fun updateSensorChart(position: Int) {
+        when (position) {
+            1 -> return // TODO: 06/04/2019 Fix the values here
+            2 -> return
+            else -> return
+        }
+    }
+
+    private fun updateWeatherChart(position: Int) {
+        when (position) {
+            1 -> return
+            2 -> {
+                // Precipitation, show the correct chart
+                weatherRainfallChart.visibility = View.VISIBLE
+                weatherChart.visibility = View.GONE
+            }
+            else -> return
+        }
+    }
 
 }
